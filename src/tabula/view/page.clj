5e26d2fe8f475634b698ea870f80f596724ffbfc
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2014-11-12 18:31:06 +0000

    Add page view

---
+++ src/tabula/view/page.clj
@@ -0,0 +1,18 @@
+(ns tabula.view.page
+  (:require [environ.core :refer [env]]
+            [hiccup.page :refer [html5 include-js include-css]]))
+
+(defn layout [body]
+  (html5 {:lang "en"}
+         [:head
+          [:meta {:charset "utf-8"}]
+          [:meta {:name "viewport"
+                  :content "width=device-width, initial-scale=1"}]
+          [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
+          [:title "Tabula"]
+          ;; TODO: include project version string
+          (include-css (str "/css/tabula.css?" (eval (env :cache-buster))))
+          "<!--[if lt IE 9]>"
+          (include-js "/js/html5shiv.3.7.2.min.js" "/js/respond.1.4.2.min.js")
+          "<![endif]-->"]
+         [:body body]))
