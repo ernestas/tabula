b50be5778309766900ec669075a8b17dabc61be1
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2014-11-13 08:30:33 +0000

    Cleanup home endpoint

--- src/tabula/endpoint/home.clj
+++ src/tabula/endpoint/home.clj
@@ -1,10 +1,7 @@
 (ns tabula.endpoint.home
   (:require [compojure.core :refer :all]
-            [tabula.model.user :as user]
             [tabula.view.home :as home]))
 
 (defn home-endpoint [{:keys [db]}]
   (routes
-   (GET "/" request (str (home/draw request db)
-                         "<br><br>" request "<br><br>"
-                         (user/get-user db (-> request :session :id))))))
+   (GET "/" request (home/draw request db))))
