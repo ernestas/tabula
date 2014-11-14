597c795876261264210fb302bd631958cf93b978
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2014-11-14 13:21:10 +0000

    Add server registration

--- src/tabula/endpoint/server.clj
+++ src/tabula/endpoint/server.clj
@@ -1,9 +1,32 @@
 (ns tabula.endpoint.server
-  (:require [compojure.core :refer :all]
-            [tabula.view.server :as server-view]))
+  (:require [ring.util.response :refer [redirect]]
+            [compojure.core :refer [context GET wrap-routes]]
+            [tabula.model.player :as player]
+            [tabula.view.server :as server-view]
+            [tabula.view.server.registration :as registration]))
+
+(defn handler [db]
+  (context
+   "/server/:server-id" [server-id]
+   (GET "/" request (server-view/draw server-id request db))
+   ;; TODO: only match a valid server names
+   (registration/routes db server-id)))
+
+(defn wrap-register
+  [db]
+  (fn [handler]
+    (fn [{{server-id :server-id} :params, {user-id :id} :session
+          request-method :request-method, uri :uri, :as request}]
+      (let [registration-route (str "/server/" server-id "/registration")
+            player-tribe (player/tribe db user-id server-id)]
+        (if (= uri registration-route)
+          (if player-tribe
+            (redirect (str "/server/" server-id))
+            (handler request))
+          (if player-tribe
+            (handler request)
+            (redirect registration-route)))))))
 
 (defn server-endpoint [{:keys [db]}]
-  (context
-   ;; TODO: only match valid server names
-   "/server/:server-id" [server-id]
-   (GET "/" request (server-view/draw server-id request db))))
+  (wrap-routes (handler db)
+               (wrap-register db)))
---
+++ src/tabula/view/server/registration.clj
@@ -0,0 +1,33 @@
+(ns tabula.view.server.registration
+  (:require [ring.util.response :refer [redirect]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [compojure.core :refer [GET POST] :as compojure]
+            [hiccup.form :refer (form-to radio-button submit-button)]
+            [hiccup.core :refer [html]]
+            [tabula.model.player :as player]
+            [tabula.view.page :as page]))
+
+(defn view
+  [uri]
+  (page/layout
+         (html [:h2 "Registration"]
+               (form-to [:post uri]
+                        (anti-forgery-field)
+                        (radio-button "tribe" false "1") "Attacking"
+                        (radio-button "tribe" false "2") "Defensive"
+                        (radio-button "tribe" false "3") "Economic"
+                        (submit-button "Play!")))))
+
+(defn routes
+  [db server-id]
+  (compojure/routes
+   (GET "/registration"
+        {uri :uri}
+        (view uri))
+   (POST "/registration"
+         [tribe :as {{user-id :id} :session, uri :uri, :as request}]
+         ;; TODO: validation
+         (if (seq tribe)
+           (do @(player/add db user-id server-id tribe)
+               (redirect (str "/server/" server-id)))
+           (view uri)))))
